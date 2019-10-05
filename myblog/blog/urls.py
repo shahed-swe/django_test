6f36d4bf96cbe0ceb562cc5e2e47012330b901ef
Md Shahed Talukder <rbrtalx@noreply.github.com>
2019-10-05 21:51:46 +0600

    Create urls.py

---
+++ myblog/blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from blog.views import(
+    blog_post_detail_page,
+    blog_post_list_view,
+    blog_post_update_view,
+    blog_post_delete_view
+)
+
+urlpatterns = [
+
+    path('',blog_post_list_view),
+    path('<str:post_slug>/',blog_post_detail_page),
+    path('<str:post_slug>/edit',blog_post_update_view),
+    path('<str:post_slug>/delete',blog_post_delete_view),
+
+]
