c7f51b357fd52db2739e3bc005b2a764201920be
Md Shahed Talukder <rbrtalx@noreply.github.com>
2019-10-06 12:01:12 +0600

    Update models.py

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -8,3 +8,4 @@ class BlogPost(models.Model):
     about = models.TextField(null=True)
     time = models.TimeField()
     date = models.DateField()
+    select = models.CharField(null = True,max_length=256, choices=[('Model Feed', 'Model Feed'),('News Feed', 'News Feed'),('Other Feed','Other Feed')])
